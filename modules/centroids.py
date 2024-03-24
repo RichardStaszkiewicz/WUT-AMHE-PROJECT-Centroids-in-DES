20711110d8c0a38047540e6d6c4c7c035de12527
Richard <richard.staszkiewicz@gmail.com>
2024-03-24 11:11:53 +0100

    [Analysis] Fully functional trials. Simple centroid plotting example.

--- modules/centroids.py
+++ modules/centroids.py
@@ -15,11 +15,14 @@ def mean_centroid(population, args=None):
 def median_centroid(population, args=None):
     return np.median(population.T, axis=0)
 
-
+#TODO: Refactor interquartile and windsor to handle changing percentiles
 def interquartile_centroid(population, args=None):
     """
     args[0] = percentile to cut (int)
     """
+    ######################
+    args[0] = args[0] if type(args[0]) == int else 20
+    ######################
     iqr = np.percentile(
         population.T, 100-args[0], axis=0) - np.percentile(population.T, args[0], axis=0)
     return np.percentile(population.T, args[0], axis=0) + iqr/2
@@ -37,6 +40,9 @@ def windsor_centroid(population, args=None):
         numpy.ndarray: Windsorized mean of the input array along the 0 axis.
     """
     def windsor_mean(population):
+        ######################
+        args[0] = args[0] if type(args[0]) == int else 10
+        ######################
         population = population.T
         lower_percentile = np.percentile(population, args[0])
         upper_percentile = np.percentile(population, 100 - args[0])
@@ -49,5 +55,4 @@ def windsor_centroid(population, args=None):
 
         # Calculate the mean
         return np.mean(population)
-
     return np.apply_along_axis(windsor_mean, 0, population.T)
