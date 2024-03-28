5c27db89c167432503793daeb126e7d658e151f2
Igor Matynia <igor.matynia@gmail.com>
2024-03-28 21:33:58 +0100

    added trial runner and tests

---
+++ modules/trial_runner.py
@@ -0,0 +1,159 @@
+from modules.DES import des_classic
+from modules.centroids import *
+import numpy as np
+import os
+from dataclasses import dataclass, field
+import multiprocessing
+from copy import copy
+
+
+RESULTS_FOLDER = './results'
+
+
+@dataclass
+class Trial:
+    """Holds information needed do perform a single separated trial
+    """
+    f: object
+    dim: int
+    centroid: callable
+    params: dict
+    x_min: np.array
+    m: float
+    constraints: np.array
+    repetition: int
+
+    def __str__(self) -> str:
+        return f"{self.f.name} @ dim={self.dim}, centr={self.centroid.__qualname__}, rep={self.repetition}"
+
+    @classmethod
+    def from_raw_f(cls, f_raw: object, dim: int, centroid: callable, rep: int, default_params: dict):
+        """Creates a single proper trial
+
+        Args:
+            f_raw (object): function object, not initialized with any dimention
+            dim (int): dimentionality
+            centroid (callable): centroid function
+            rep (int): repetition NUMBER (which repetion is this?)
+            default_params (dict): default params
+        """
+        f = f_raw(dim)
+        x_min, m = f.get_global_minimum(dim)
+
+        params = copy(default_params)
+        params["lower"] = f.input_domain.T[0]
+        params["upper"] = f.input_domain.T[1]
+
+        constraints = f.input_domain.T[0]
+
+        params_centroid = copy(params)
+        params_centroid['centroid_fc'] = centroid
+
+        return cls(
+            f,
+            dim,
+            centroid,
+            params_centroid,
+            x_min,
+            m,
+            constraints,
+            rep
+        )
+
+
+@dataclass
+class TrialConfig:
+    """Holds experiments to be run in parallel by the trial runner
+    """
+    experiments: list[Trial] = field(default_factory=list)
+
+    @classmethod
+    def from_product(cls, funcs: list[object], dims: list[int], centroids: list[callable], default_params: dict, repetitions: int):
+        """Creates a config object, which constains a list of experiments which are all possible combinations of given functions, dimentions and centroids. Each combination is multiplied repetitions times.
+
+        Args:
+            funcs (list[object]): list of functions to consider
+            dims (list[int]): list of dimentions
+            centroids (list[callable]): list of centroid funcs
+            default_params (dict): default parameters for DES
+            repetitions (int): number of repetitions of each combination
+
+        Returns:
+            TrialConfig: the resulting trial config
+        """
+        config = cls()
+        for f_raw in funcs:
+            for dim in dims:
+                if not f_raw.is_dim_compatible(dim):
+                    print(f"{f_raw.name} incompatible with dim={dim}")
+                    continue
+                for rep in range(repetitions):
+                    for centroid in centroids:
+                        try:
+                            config.experiments.append(
+                                Trial.from_raw_f(
+                                    f_raw,
+                                    dim,
+                                    centroid,
+                                    rep,
+                                    default_params
+                                ))
+                        except Exception as e:
+                            # print(f"{e}")
+                            pass
+        return config
+
+
+@dataclass
+class TrialResult:
+    """Holds trial results
+    """
+    trial: Trial
+    logs: object
+
+    def get_dump_path(self):
+        return f'{RESULTS_FOLDER}/{self.get_descriptor()}'
+
+    def get_descriptor(self):
+        return f"{self.trial.centroid.__qualname__};{self.trial.f.name};{self.trial.dim};{self.trial.repetition}"
+
+    @classmethod
+    def get_path_from_parts(cls, centroid: callable, f: object, dim: int, repetition: int):
+        """Creates a dump path from component parts to read from
+
+        Args:
+            centroid (callable): centroid function
+            f (object): function object
+            dim (int): dimentionality
+            repetition (int): repetition number
+        """
+        descriptor = f"{centroid.__qualname__};{f.name};{dim};{repetition}"
+        return f'{RESULTS_FOLDER}/{descriptor}'
+
+
+class TrialRunner:
+    BASE_SALT: int = 999
+
+    def __init__(self, trial_config: TrialConfig, max_procs: int = multiprocessing.cpu_count()) -> None:
+        self._max_procs = min(multiprocessing.cpu_count(), max_procs)
+        self._config = trial_config
+
+    def run_all(self):
+        try:
+            os.mkdir(RESULTS_FOLDER)
+        except FileExistsError as _:
+            pass
+
+        with multiprocessing.Pool() as pool:
+            pool.map(self.run_one_experiment, self._config.experiments)
+
+    @classmethod
+    def run_one_experiment(cls, trial: Trial):
+        print(f"Processing {trial}\n", end="")
+        np.random.seed(cls.BASE_SALT + trial.repetition)
+        result = des_classic(trial.constraints, trial.f, **trial.params)
+        cls._save_result(TrialResult(trial, result))
+
+    @classmethod
+    def _save_result(cls, result: TrialResult):
+        np.save(result.get_dump_path(), result.logs, allow_pickle=True)
---
+++ tests/test_trial_runner.py
@@ -0,0 +1,45 @@
+from modules.trial_runner import *
+import Python_Benchmark_Test_Optimization_Function_Single_Objective.pybenchfunction as bench
+
+DEFAULT_PARAMS = {
+    "upper": np.array([-5, -23.3, 14, 11]),
+    "lower": np.array([-101, -101, -101, -150]),
+    "stopfitness": 1e-10,
+    "lambda": 5,
+    "time": 5,
+    "centroid_fc": mean_centroid,
+    "diag": True,
+}
+
+
+class TestTrials:
+    def test_trial_init_from_raw(self):
+        bench_funcs = bench.get_functions(None)[0:3]
+        trial = Trial.from_raw_f(
+            bench_funcs[0], 2, ALL_CENTROIDS[0], 0, DEFAULT_PARAMS
+        )
+        assert str(trial) == 'Ackley @ dim=2, centr=vanila_centroid, rep=0'
+
+    def test_run_example_experiment(self):
+        bench_funcs = bench.get_functions(None)[0:3]
+        trial = Trial.from_raw_f(
+            bench_funcs[0], 2, ALL_CENTROIDS[0], 0, DEFAULT_PARAMS
+        )
+
+        TrialRunner.run_one_experiment(trial)
+
+
+class TestConfig:
+    def test_init_from_product(self):
+        bench_funcs = bench.get_functions(None)
+
+        config = TrialConfig.from_product(
+            list(filter((lambda func: func.is_dim_compatible(50)), bench_funcs))[
+                0:3],
+            [2, 10, 50],
+            ALL_CENTROIDS[0:3],
+            DEFAULT_PARAMS,
+            5
+        )
+
+        assert len(config.experiments) == 3*3*3*5
